0e3ecaa5817895df362b949fb1974391bc536a2e
xchanmolxoaw4d <huanyid894885@yeah.net>
2021-08-28 00:21:00 +0800

    init

--- apidoc-demo/src/main/java/com/demo/conf/ApiDocConfig.java
+++ apidoc-demo/src/main/java/com/demo/conf/ApiDocConfig.java
@@ -16,13 +16,16 @@ import java.io.File;
  * 让springboot自动扫描并管理apidoc工具下的所有class
  */
 @Configuration
-@EnableTransactionManagement
-@ComponentScan("com.apidoc")
-@MapperScan("com.apidoc.dao")
-@EntityScan("com.apidoc.entity")
+@EnableTransactionManagement//启动事务
+@ComponentScan("com.apidoc")//扫描组件类
+@MapperScan("com.apidoc.dao")//扫描数据库操作层的类
+@EntityScan("com.apidoc.entity")//扫描实体类
 public class ApiDocConfig {
     @Bean
     public ApiDocService generator() {
+        //所有的常量都在Const类下，需要修改常量的在这里配置即可
+        //配置代码的绝对路径，方便扫描代码的注释，因为注释编译之后就被jvm剔除了，只能扫描源码，不配置则不扫描注释
+        //默认路径为{项目路径}+src/main/java
         Const.codePath = Const.projectPath + "apidoc-demo" + File.separator + "src" + File.separator + "main" + File.separator + "java" + File.separator;
         return new ApiDocService();
     }
